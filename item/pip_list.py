0577a2287c94171d6e6fa019187db3f81299a4ef
Shock-Nin <shocknin7@gmail.com>
2022-02-06 20:26:30 +0900

    pip

---
+++ item/pip_list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# /Users/dsk_nagaoka/.pyenv/versions/3.9.6/bin/python -m pip install --upgrade pip
+
+import cv2
+import PySimpleGUI
+import pyautogui
+import numpy
+import PIL
+import Pillow
+import pandas
+
