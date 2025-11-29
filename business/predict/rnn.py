2cf0f54433ed80cacfb5d794b1e510cf50b64d7c
Shock-Nin <shocknin7@gmail.com>
2025-11-30 06:38:28 +0900

    モデル作成 DEV

--- business/predict/rnn.py
+++ business/predict/rnn.py
@@ -20,7 +20,7 @@ import matplotlib.pyplot as plt
 plt.style.use('fivethirtyeight')
 
 # データとしては約四年分あるが、今回はこれを8このパートに分けて、それぞれの領域で予想を行う
-TERM_PART_LIST = [0, 120, 240, 360, 480, 600, 720, 840]
+TERM_PART = [1, 120]
 
 # 予測に利用するデータ数
 # 90個のデータから後の30個のデータを予測する
@@ -30,12 +30,11 @@ NUM_LSTM = 90
 NUM_MIDDLE = 200
 
 
-def create(dict_df, inputs):
-    com.log('RNNモデル作成開始')
+def create(model_type, dict_df, inputs):
+    com.log(model_type + 'モデル作成開始')
 
-    fig, ax = plt.subplots(1, len(dict_df), figsize=(16, 6))
-    fig.suptitle('RNN [' + ', '.join(currency.replace('USD', '') for currency in dict_df) + ']')
-
+    fig, ax = plt.subplots(1, len(dict_df), figsize=(cst.FIG_SIZE))
+    fig.suptitle(model_type + ' [' + ', '.join(currency.replace('USD', '') for currency in dict_df) + ']')
 
     total_time = 0
     cnt = 0
@@ -43,7 +42,7 @@ def create(dict_df, inputs):
         for currency in dict_df:
             start_time = com.time_start()
 
-            window = com.progress('RNNモデル作成中', [currency, len(dict_df)], interrupt=True)
+            window = com.progress(model_type + 'モデル作成中', [currency, len(dict_df)], interrupt=True)
             event, values = window.read(timeout=0)
 
             window[currency].update(currency + ' (' + str(cnt) + ' / ' + str(len(dict_df)) + ')')
@@ -51,23 +50,20 @@ def create(dict_df, inputs):
 
             df = dict_df[currency]
 
+            for term in range(1, TERM_PART[0] + 1):
 
-            for term in [1, 2, 3, 4, 5, 6]:
                 n, date, x, y, l_x, l_y = _get_x_y_lx_ly(df, term)
-                model = _build_model('RNN')
-                # model = build_model('LSTM')
-                # model = build_model('GRU')
-                history = model.fit(l_x, l_y, epochs=int(inputs[1]), batch_size=32, validation_split=0.25,
-                                    verbose=0)
-                print('予測期間 : {} - {} の結果'.format(date[0], date[NUM_LSTM - 1]))
-                _plot_result(model, n, date, x, y, l_x, l_y)
+                model = _build_model(model_type)
+                history = model.fit(l_x, l_y, epochs=int(inputs[1]), batch_size=32,
+                                    validation_split=0.25, verbose=0)
 
+                _plot_result(ax, cnt, model, n, date, x, y, l_x, l_y)
 
             loss = history.history['loss']
             val_loss = history.history['val_loss']
 
-            ax[cnt].plot(np.arange(len(loss)), loss, label='loss')
-            ax[cnt].plot(np.arange(len(val_loss)), val_loss, label='val_loss')
+            # ax[cnt].plot(np.arange(len(loss)), loss, label='loss')
+            # ax[cnt].plot(np.arange(len(val_loss)), val_loss, label='val_loss')
 
             cnt += 1
             window.close()
@@ -76,10 +72,11 @@ def create(dict_df, inputs):
             total_time += run_time
             com.log(currency + '予測完了(' + com.conv_time_str(run_time) + ') ')
 
+        plt.xticks(date[::12], rotation=60)
         plt.gcf().autofmt_xdate()
         plt.grid()
         # plt.legend(['Train', 'Val', 'Predictions'], loc='lower center')
-        plt.legend()
+        # plt.legend()
         plt.show()
     finally:
         try: window.close()
@@ -88,12 +85,13 @@ def create(dict_df, inputs):
     com.log('RNNモデル作成完了(' + com.conv_time_str(total_time) + ')')
 
 def _get_x_y_lx_ly(df, term_part):
-    print(df)
-    date = np.array(df.index[TERM_PART_LIST[term_part]: TERM_PART_LIST[term_part + 1]])
-    x = np.array(df.index.values[TERM_PART_LIST[term_part]: TERM_PART_LIST[term_part + 1]])
-    y = np.array(df['Close'][TERM_PART_LIST[term_part]: TERM_PART_LIST[term_part + 1]])
+
+    date = np.array(df.index[TERM_PART[1] * term_part: TERM_PART[1] * (1 + term_part)])
+    x = np.array(df.index.values[TERM_PART[1] * term_part: TERM_PART[1] * (1 + term_part)])
+    y = np.array(df['Close'][TERM_PART[1] * term_part: TERM_PART[1] * (1 + term_part)])
 
     n = len(y) - NUM_LSTM
+
     l_x = np.zeros((n, NUM_LSTM))
     l_y = np.zeros((n, NUM_LSTM))
 
@@ -105,6 +103,40 @@ def _get_x_y_lx_ly(df, term_part):
     l_y = l_y.reshape(n, NUM_LSTM, 1)
 
     return n, date, x, y, l_x, l_y
+
+def _plot_result(ax, cnt, model, n, date, x, y, l_x, l_y):
+
+    # 初期の入力値
+    res = []
+    res = np.append(res, l_x[0][0][0])
+    res = np.append(res, l_y[0].reshape(-1))
+
+    for i in range(0, n):
+        _y = model.predict(res[- NUM_LSTM:].reshape(1, NUM_LSTM, 1))
+
+        # 予測されたデータを次の予測のためのインプットデータとして利用
+        res = np.append(res, _y[0][NUM_LSTM - 1][0])
+
+    res = np.delete(res, -1)
+
+    ax[cnt].plot(date, y, label='stock price', color='coral')
+    ax[cnt].plot(date, res, label='prediction result', color='blue')
+
+    # ax[cnt].xticks(date[::12], rotation=60)
+
+    # ax[cnt].legend()
+    ax[cnt].grid()
+
+    ax[cnt].axvspan(0, NUM_LSTM, color='coral', alpha=0.2)
+
+    # plt.show()
+
+# 中断イベント
+def _is_interrupt(window, event):
+    if event in [sg.WIN_CLOSED, 'interrupt']:
+        window.close()
+        return True
+    return False
 
 
 def _build_model(model_name='RNN'):
@@ -121,42 +153,7 @@ def _build_model(model_name='RNN'):
     if model_name == 'GRU':
         model.add(GRU(NUM_MIDDLE, input_shape=(NUM_LSTM, 1), return_sequences=True))
 
-    model.add(Dense(1, activation="linear"))
-    model.compile(loss="mean_squared_error", optimizer="sgd")
+    model.add(Dense(1, activation='linear'))
+    model.compile(loss='mean_squared_error', optimizer='sgd')
 
     return model
-
-def _plot_result(model, n, date, x, y, l_x, l_y):
-
-    # 初期の入力値
-    res = []
-    res = np.append(res, l_x[0][0][0])
-    res = np.append(res, l_y[0].reshape(-1))
-
-    for i in range(0, n):
-        _y = model.predict(res[- NUM_LSTM:].reshape(1, NUM_LSTM, 1))
-
-        # 予測されたデータを次の予測のためのインプットデータとして利用
-        res = np.append(res, _y[0][NUM_LSTM - 1][0])
-
-    res = np.delete(res, -1)
-
-    plt.plot(date, y, label="stock price", color='coral')
-    plt.plot(date, res, label="prediction result", color='blue')
-    plt.xticks(date[::12], rotation=60)
-
-    plt.legend()
-    plt.grid()
-
-    plt.axvspan(0, NUM_LSTM, color="coral", alpha=0.2)
-
-    plt.show()
-
-    print('{} - {} の結果'.format(date[0], date[NUM_LSTM - 1]))
-
-# 中断イベント
-def _is_interrupt(window, event):
-    if event in [sg.WIN_CLOSED, 'interrupt']:
-        window.close()
-        return True
-    return False
